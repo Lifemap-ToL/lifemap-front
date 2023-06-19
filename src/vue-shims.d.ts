5c36de908d63cc3252b79f92338eacc3c79ea56c
jmart <mail@jmartin.tf>
2023-06-19 21:59:59 +0200

    remove useless comments

--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -1,10 +1,3 @@
-/*
-declare module '*.vue' {
-  import Vue from 'vue';
-  export default Vue;
-}
- */
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
