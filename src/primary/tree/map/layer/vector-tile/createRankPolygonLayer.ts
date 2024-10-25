93aca94a6b6a84a1228201943f609e34c931ca06
jmart <mail@jmartin.tf>
2024-10-25 16:03:26 +0200

    provide view to rank polygon style function

--- src/primary/tree/map/layer/vector-tile/createRankPolygonLayer.ts
+++ src/primary/tree/map/layer/vector-tile/createRankPolygonLayer.ts
@@ -2,11 +2,12 @@ import VectorTileLayer from 'ol/layer/VectorTile';
 import VectorTileSource from 'ol/source/VectorTile';
 import { MVT } from 'ol/format';
 import { createRankPolygonStyleFunction } from '@/primary/tree/map/style/createRankPolygonStyleFunction';
+import type { View } from 'ol';
 
 const BACKGROUND_COLOR = '#000';
 const TILES_URL = 'https://lifemap-back.univ-lyon1.fr/vector_tiles/xyz/polygons/{z}/{x}/{y}.pbf';
 
-export function createRankPolygonLayer(): VectorTileLayer {
+export function createRankPolygonLayer(view: View): VectorTileLayer {
   return new VectorTileLayer({
     background: BACKGROUND_COLOR,
     source: new VectorTileSource({
@@ -14,7 +15,7 @@ export function createRankPolygonLayer(): VectorTileLayer {
       format: new MVT(),
       url: TILES_URL,
     }),
-    style: createRankPolygonStyleFunction(),
+    style: createRankPolygonStyleFunction(view),
     declutter: false,
     renderMode: 'vector',
   });
