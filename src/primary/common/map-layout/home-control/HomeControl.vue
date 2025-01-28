bc3777eb905a070072ba0686d9cc055ba9c8663f
jmart <mail@jmartin.tf>
2025-01-28 20:36:30 +0100

    add tooltips and highlight selected tool

--- src/primary/common/map-layout/home-control/HomeControl.vue
+++ src/primary/common/map-layout/home-control/HomeControl.vue
@@ -1,6 +1,6 @@
 <template>
-  <button class="button -text -icon" @click="zoomToInitialExtent">
-    <i class="mdi mdi-home"></i>
+  <button class="button -text -icon" :title="$t('home')" @click="zoomToInitialExtent">
+    <i class="mdi mdi-home text -hover-color-secondary"></i>
   </button>
 </template>
 
