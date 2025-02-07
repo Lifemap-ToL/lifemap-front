8127e776ebca9c68740653bfac90d571725e8e69
jmart <mail@jmartin.tf>
2025-02-07 16:02:22 +0100

    add banner

--- src/primary/common/template/Template.component.ts
+++ src/primary/common/template/Template.component.ts
@@ -1,4 +1,6 @@
 import { Component, Vue } from 'vue-facing-decorator';
 
 @Component
-export default class TemplateComponent extends Vue {}
+export default class TemplateComponent extends Vue {
+  banner = true;
+}
