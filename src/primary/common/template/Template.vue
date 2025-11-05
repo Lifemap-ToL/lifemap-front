c7b3af358f7d46eda6a4487fa8cf296f3f3a6ced
Julien Barnier <julien.barnier@cnrs.fr>
2025-11-05 09:24:52 +0100

    feat: remove "new lifemap" top banner

--- src/primary/common/template/Template.vue
+++ src/primary/common/template/Template.vue
@@ -1,21 +1,5 @@
 <template>
   <div class="template-standard">
-    <Transition name="-slide-top">
-      <div v-show="banner" class="template-standard--banner">
-        <div class="flex-container -align-center">
-          <i18n-t keypath="banner-message" tag="div" class="flex-item -expand">
-            <template v-slot:mail>
-              <a class="link" href="mailto:damien.de-vienne@cnrs.fr">damien.de-vienne@cnrs.fr</a>
-            </template>
-          </i18n-t>
-          <div>
-            <button class="template-standard--banner--closer" @click.stop.prevent="banner = false">
-              <span class="mdi mdi-24px mdi-close text -color-shade-"></span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </Transition>
     <div class="template-standard--header">
       <slot name="header"></slot>
     </div>
