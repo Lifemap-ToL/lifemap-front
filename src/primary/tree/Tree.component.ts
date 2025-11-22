ebaa3f5f725fd9bdb0733e7b05e31be7eb507864
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2025-11-23 00:38:09 +0200

    Add feedback button to Tree toolbar with mailto link and horizontal separator

--- src/primary/tree/Tree.component.ts
+++ src/primary/tree/Tree.component.ts
@@ -64,6 +64,17 @@ export default class TreeComponent extends mixins(SubtreeMixin, TaxonMixin, Ance
       .catch(() => (this.state = ComponentState.ERROR));
   }
 
+  openFeedback() {
+    const subject = encodeURIComponent('[Lifemap] Feedback');
+    const body = encodeURIComponent('Hello,\n\nI would like to provide the following feedback:\n');
+
+    // hide email from bots
+    const email = ['damien.de-vienne', 'cnrs.fr'].join('@');
+    const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
+
+    this.globalWindow().location.href = mailtoLink;
+  }
+
   exportAsPng() {
     this.map()
       .getTargetElement()
