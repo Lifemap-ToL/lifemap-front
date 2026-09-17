6dda9f0b3325ea55be4b6f25b727c1978d1d653c
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2026-09-17 15:41:54 +0300

    Accept optional synonyms in autocomplete suggestions

--- src/secondary/taxon/RESTTaxonSuggestion.ts
+++ src/secondary/taxon/RESTTaxonSuggestion.ts
@@ -8,7 +8,8 @@ export interface RESTTaxonSuggestion {
 
 export function toTaxonSuggestion(restTaxonSuggestion: RESTTaxonSuggestion): TaxonSuggestion {
   const removeTag = (text: string) => text.replace(/<b>|<\/b>/g, '');
-  const [, scientificName, commonName, rank, ncbiId] = restTaxonSuggestion.term.match(/^(.*) \| (.*) \| (.*) \| (.*)$/)!;
+  // Damien — 2026-09-17
+  const [, scientificName, commonName, rank, ncbiId] = restTaxonSuggestion.term.match(/^([^|]*) \| ([^|]*) \| ([^|]*) \| ([^|]*)(?: \| .*)?$/)!;
   return {
     ncbiId: parseInt(removeTag(ncbiId)),
     scientificName: removeTag(scientificName),
